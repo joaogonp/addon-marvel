9464a7d80ba0b6058c9c6a685c04ccc3f4854ff1
João <jpnapsp@gmail.com>
2025-05-08 19:57:03 +0100

    1.1.0
    1.1.0

---
+++ Data/animationsData.js
@@ -0,0 +1,578 @@
+module.exports = [
+  {
+    "title": "Spider-Man",
+    "type": "series",
+    "imdbId": "tt0061301",
+    "id": "marvel_tt0061301",
+    "releaseYear": "1967",
+    "poster": "https://image.tmdb.org/t/p/original/y85QOYMhcZsEfHbrntBv5SZXH1w.jpg"
+  },
+  {
+    "title": "Fantastic Four",
+    "type": "series",
+    "imdbId": "tt0061250",
+    "id": "marvel_tt0061250",
+    "releaseYear": "1967",
+    "poster": "https://image.tmdb.org/t/p/original/8zwysNUXr5z3mDZSFEv1fZ8qo08.jpg"
+  },
+  {
+    "title": "Fantastic Four",
+    "type": "series",
+    "imdbId": "tt0241100",
+    "id": "marvel_tt0241100",
+    "releaseYear": "1978",
+    "poster": "https://image.tmdb.org/t/p/original/8Sm8WQ9Gm7fyJBimix2a05Cj6ON.jpg"
+  },
+  {
+    "title": "The Thing",
+    "type": "series",
+    "imdbId": "tt0077091",
+    "id": "marvel_tt0077091",
+    "releaseYear": "1979",
+    "poster": "https://image.tmdb.org/t/p/original/eDyeUNRBCIn2GeS4hIM0HT3xDBw.jpg"
+  },
+  {
+    "title": "Spider-Woman",
+    "type": "series",
+    "imdbId": "tt0197911",
+    "id": "marvel_tt0197911",
+    "releaseYear": "1979",
+    "poster": "https://image.tmdb.org/t/p/original/cXiD1jVLjTyvO3w2jKq9QdlAYag.jpg"
+  },
+  {
+    "title": "Spider-Man",
+    "type": "series",
+    "imdbId": "tt0081938",
+    "id": "marvel_tt0081938",
+    "releaseYear": "1981",
+    "poster": "https://image.tmdb.org/t/p/original/5xZac4wWUlXQmEBqIIKJ9rqXnUh.jpg"
+  },
+  {
+    "title": "Spider-Man and His Amazing Friends",
+    "type": "series",
+    "imdbId": "tt0083481",
+    "id": "marvel_tt0083481",
+    "releaseYear": "1981",
+    "poster": "https://image.tmdb.org/t/p/original/4b8HFH1arZ4b05RHwxPc2JfueYv.jpg"
+  },
+  {
+    "title": "Incredible Hulk",
+    "type": "series",
+    "imdbId": "tt0083430",
+    "id": "marvel_tt0083430",
+    "releaseYear": "1982",
+    "poster": "https://image.tmdb.org/t/p/original/eg1HgM1kF0SJbYsWVmpP2BjvPUU.jpg"
+  },
+  {
+    "title": "X-Men",
+    "type": "series",
+    "imdbId": "tt0103584",
+    "id": "marvel_tt0103584",
+    "releaseYear": "1992",
+    "poster": "https://image.tmdb.org/t/p/original/2OFwsFhAobczgJWnVdwd9oZAF2N.jpg"
+  },
+  {
+    "title": "Iron Man",
+    "type": "series",
+    "imdbId": "tt0115218",
+    "id": "marvel_tt0115218",
+    "releaseYear": "1994",
+    "poster": "https://image.tmdb.org/t/p/original/nTQWWH6CFtl37x1nPx8HRwbwvGn.jpg"
+  },
+  {
+    "title": "Fantastic Four",
+    "type": "series",
+    "imdbId": "tt0187635",
+    "id": "marvel_tt0187635",
+    "releaseYear": "1994",
+    "poster": "https://image.tmdb.org/t/p/original/jy7fvS9ow4MadFZuhwadshj0yes.jpg"
+  },
+  {
+    "title": " Spider-Man",
+    "type": "series",
+    "imdbId": "tt0112175",
+    "id": "marvel_tt0112175",
+    "releaseYear": "1994",
+    "poster": "https://image.tmdb.org/t/p/original/wXthtEN5kdWA1bHz03lkuCJS6hA.jpg"
+  },
+  {
+    "title": "UltraForce",
+    "type": "series",
+    "imdbId": "tt0484812",
+    "id": "marlve_tt0484812",
+    "releaseYear": "1995",
+    "poster": "https://image.tmdb.org/t/p/original/i0fI892hW938Zk1RA1vpqVmI5fS.jpg"
+  },
+  {
+    "title": "The Incredible Hulk",
+    "type": "series",
+    "imdbId": "tt0115215",
+    "id": "marvel_tt0115215",
+    "releaseYear": "1996",
+    "poster": "https://image.tmdb.org/t/p/original/wa60s9NSvrH87NFCArmobnIyNnh.jpg"
+  },
+  {
+    "title": "Men in Black",
+    "type": "series",
+    "imdbId": "tt0127385",
+    "id": "marlve_tt0127385",
+    "releaseYear": "1997",
+    "poster": "https://image.tmdb.org/t/p/original/hmXG0SBVg0vjD4xp3jWQ0cZJIOr.jpg"
+  },
+  {
+    "title": "Silver Surfer",
+    "type": "series",
+    "imdbId": "tt0203268",
+    "id": "marvel_tt0203268",
+    "releaseYear": "1998",
+    "poster": "https://image.tmdb.org/t/p/original/18rhdRDXAAHUTsbWtIQn2ZkjRL9.jpg"
+  },
+  {
+    "title": "Spider-Man Unlimited",
+    "type": "series",
+    "imdbId": "tt0207120",
+    "id": "marlvel_tt0207120",
+    "releaseYear": "1999",
+    "poster": "https://image.tmdb.org/t/p/original/hNdf1WmFil9IV7rabBe6PnAmYnI.jpg"
+  },
+  {
+    "title": "Avengers: United They Stand",
+    "type": "series",
+    "imdbId": "tt0203247",
+    "id": "marvel_tt0203247",
+    "releaseYear": "1999",
+    "poster": "https://image.tmdb.org/t/p/original/p2SrnKTQjLRXBCcTZtYkTZCwLpp.jpg"
+  },
+  {
+    "title": "X-Men: Evolution",
+    "type": "series",
+    "imdbId": "tt0247827",
+    "id": "marvel_tt0247827",
+    "releaseYear": "2000",
+    "poster": "https://image.tmdb.org/t/p/original/At5aZIKkN8zHTAXOnQR9D3b7usW.jpg"
+  },
+  {
+    "title": "Spider-Man: The New Animated Series",
+    "type": "series",
+    "imdbId": "tt0290983",
+    "id": "marvel_tt0290983",
+    "releaseYear": "2003",
+    "poster": "https://image.tmdb.org/t/p/original/8jLRJnMcSwIZOm1lKBU4pV6zM2M.jpg"
+  },
+  {
+    "title": "Fantastic Four: World's Greatest Heroes",
+    "type": "series",
+    "imdbId": "tt0830298",
+    "id": "marvel_tt0830298",
+    "releaseYear": "2006",
+    "poster": "https://image.tmdb.org/t/p/original/tADUrw8v8YsFPVTikxughgP2wrL.jpg"
+  },
+  {
+    "title": "Ultimate Avengers",
+    "type": "movie",
+    "imdbId": "tt0491703",
+    "id": "marvel_tt0773262",
+    "releaseYear": "2006",
+    "poster": "https://image.tmdb.org/t/p/original/zyMZ2boSccbaUjOkl6AdzLb6KKd.jpg"
+  },
+  {
+    "title": "X-Men: Darktide",
+    "type": "movie",
+    "imdbId": "tt2949494",
+    "id": "marvel_tt2949494",
+    "releaseYear": "2006",
+    "poster": "https://image.tmdb.org/t/p/original/2233WXs0oggdnb8IGOpBgmBQnBq.jpg"
+  },
+  {
+    "title": "Ultimate Avengers 2",
+    "type": "movie",
+    "imdbId": "tt0803093",
+    "id": "marvel_tt0803093",
+    "releaseYear": "2006",
+    "poster": "https://image.tmdb.org/t/p/original/2bfVNvLKWaxFh1LiOKlvxgiHOXn.jpg"
+  },
+  {
+    "title": "The Invincible Iron Man",
+    "type": "movie",
+    "imdbId": "tt0903135",
+    "id": "marvel_tt0903135",
+    "releaseYear": "2007",
+    "poster": "https://image.tmdb.org/t/p/original/eebNTSRa5Hh5skPKGdGJIJpo9Ls.jpg"
+  },
+  {
+    "title": "Doctor Strange: The Sorcerer Supreme",
+    "type": "movie",
+    "imdbId": "tt0910865",
+    "id": "marvel_tt0910865",
+    "releaseYear": "2007",
+    "poster": "https://image.tmdb.org/t/p/original/oczXYdStbFituTIDharMhcXDAkg.jpg"
+  },
+  {
+    "title": "Spectacular Spider-Man",
+    "type": "series",
+    "imdbId": "tt0976192",
+    "id": "marvel_tt0976192",
+    "releaseYear": "2008",
+    "poster": "https://image.tmdb.org/t/p/original/gZW6uJR2f6pLmjRWABfEY4Q7VuQ.jpg"
+  },
+  {
+    "title": "Wolverine and the X-Men",
+    "type": "series",
+    "imdbId": "tt0772145",
+    "id": "m,arvel_tt0772145",
+    "releaseYear": "2008",
+    "poster": "https://image.tmdb.org/t/p/original/oSeJ2nESB77iC6SLo4qcqoadjdt.jpg"
+  },
+  {
+    "title": "Iron Man: Armored Adventures",
+    "type": "series",
+    "imdbId": "tt0837143",
+    "id": "marvel_tt0837143",
+    "releaseYear": "2008",
+    "poster": "https://image.tmdb.org/t/p/original/kIJGgBwh37V2nfgnJFaN7zDIjdv.jpg"
+  },
+  {
+    "title": "Next Avengers: Heroes of Tomorrow",
+    "type": "movie",
+    "imdbId": "tt1259998",
+    "id": "marvel_tt1259998",
+    "releaseYear": "2008",
+    "poster": "https://image.tmdb.org/t/p/original/qp66vgUHDRaeFVvNIazrxQBI33h.jpg"
+  },
+  {
+    "title": "Super Hero Squad Show",
+    "type": "series",
+    "imdbId": "tt1388589",
+    "id": "marvel_tt1388589",
+    "releaseYear": "2009",
+    "poster": "https://image.tmdb.org/t/p/original/3CdaaPZdU1Xv2c6hDaSVWhsXtuO.jpg"
+  },
+  {
+    "title": "Hulk Vs.",
+    "type": "movie",
+    "imdbId": "tt1325753",
+    "id": "marvel_tt1325753",
+    "releaseYear": "2009",
+    "poster": "https://image.tmdb.org/t/p/original/gQw25SDF8vQYC7JRD3xW0V6YCjR.jpg"
+  },
+  {
+    "title": "The Avengers: Earth's Mightiest Heroes",
+    "type": "series",
+    "imdbId": "tt1626038",
+    "id": "tt1626038",
+    "releaseYear": "2010",
+    "poster": "https://image.tmdb.org/t/p/original/nH1ROb4ujGRuDItJyn5UrYY5uOB.jpg"
+  },
+  {
+    "title": "Planet Hulk",
+    "type": "movie",
+    "imdbId": "tt1483025",
+    "id": "marvel_tt1483025",
+    "releaseYear": "2010",
+    "poster": "https://image.tmdb.org/t/p/original/5wCzY3sAs6zHIzzHIcTKN2g0pu8.jpg"
+  },
+  {
+    "title": "Thor: Tales of Asgard",
+    "type": "movie",
+    "imdbId": "tt1667903",
+    "id": "marvel_tt1667903",
+    "releaseYear": "2011",
+    "poster": "https://image.tmdb.org/t/p/original/oh8jjEBbL0BEGT3Fy2DKhbE2D58.jpg"
+  },
+  {
+    "title": "Ultimate Spide-Man",
+    "type": "series",
+    "imdbId": "tt1722512",
+    "id": "marvel_tt1722512",
+    "releaseYear": "2012",
+    "poster": "https://image.tmdb.org/t/p/original/p276PkRJ5Wexq91DA5RkbxTYMDn.jpg"
+  },
+  {
+    "title": "The Avengers: Assemble",
+    "type": "series",
+    "imdbId": "tt2455546",
+    "id": "marvel_tt2455546",
+    "releaseYear": "2012",
+    "poster": "https://image.tmdb.org/t/p/original/aVYPq7FSvsdNQOXVIFnndXJPCXn.jpg"
+  },
+  {
+    "title": "Hulk and the Agents of S.M.A.S.H.",
+    "type": "series",
+    "imdbId": "tt2455514",
+    "id": "marvel_tt2455514",
+    "releaseYear": "2013",
+    "poster": "https://image.tmdb.org/t/p/original/xxJbuRwU3ssxxrxrI38URtLkf6L.jpg"
+  },
+  {
+    "title": "Iron Man: Rise of Technovore",
+    "type": "movie",
+    "imdbId": "tt2654124",
+    "id": "marvel_tt2654124",
+    "releaseYear": "2013",
+    "poster": "https://image.tmdb.org/t/p/original/eHDez1uN5X2ZAq4niX7HvhyZIIO.jpg"
+  },
+  {
+    "title": "Iron Man and Hulk: Heroes United",
+    "type": "movie",
+    "imdbId": "tt3221698",
+    "id": "marvel_tt3221698",
+    "releaseYear": "2013",
+    "poster": "https://image.tmdb.org/t/p/original/e7F2ZNA7wMneoSKGonLKjDmjFEd.jpg"
+  },
+  {
+    "title": "Marvel Disk Wars: The Avengers",
+    "type": "series",
+    "imdbId": "tt3644256",
+    "id": "marvel_tt3644256",
+    "releaseYear": "2014",
+    "poster": "https://image.tmdb.org/t/p/original/6wQAZaWSvBJOIIt68cqmC0KJjeE.jpg"
+  },
+  {
+    "title": "Big Hero 6",
+    "type": "movie",
+    "imdbId": "tt2245084",
+    "id": "marvel_tt2245084",
+    "releaseYear": "2014",
+    "poster": "https://image.tmdb.org/t/p/original/2mxS4wUimwlLmI1xp6QW6NSU361.jpg"
+  },
+  {
+    "title": "Avengers Confidential: Black Widow & Punisher",
+    "type": "movie",
+    "imdbId": "tt3482378",
+    "id": "marvel_tt3482378",
+    "releaseYear": "2014",
+    "poster": "https://image.tmdb.org/t/p/original/wJcyYVRPZDOZOD2w0r2R6wuZqqN.jpg"
+  },
+  {
+    "title": "Iron Man & Captain America: Heroes United",
+    "type": "movie",
+    "imdbId": "tt3911200",
+    "id": "marvel_tt3911200",
+    "releaseYear": "2014",
+    "poster": "https://image.tmdb.org/t/p/original/AcFFHZGkpLUs31bKf42z8UVLimS.jpg"
+  },
+  {
+    "title": "Guardians of the Galaxy",
+    "type": "series",
+    "imdbId": "tt4176370",
+    "id": "marvel_tt4176370",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/original/naeUEBhotiD5S9lobgV7dsuuGEk.jpg"
+  },
+  {
+    "title": "Super Hero Adventures: Frost Fight!",
+    "type": "movie",
+    "imdbId": "tt5272052",
+    "id": "marvel_tt5272052",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/original/h7pgMFwnx2dhSZypbBYdoVCV1eT.jpg"
+  },
+  {
+    "title": "Hulk: Where Monsters Dwell",
+    "type": "movie",
+    "imdbId": "tt6173902",
+    "id": "marvel_tt6173902",
+    "releaseYear": "2016",
+    "poster": "https://image.tmdb.org/t/p/original/luT68rZr9wtrvn22VnRl82dkWTY.jpg"
+  },
+  {
+    "title": "Red Sonja: Queen of Plagues",
+    "type": "movie",
+    "imdbId": "tt5989606",
+    "id": "marvel_ttt5989606",
+    "releaseYear": "2016",
+    "poster": "https://image.tmdb.org/t/p/original/rOwoxKn4aW5LDhfuI0TfpGpucbO.jpg"
+  },
+  {
+    "title": "Future Avengers",
+    "type": "series",
+    "imdbId": "tt6591446",
+    "id": "marvel_tt6591446",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/g8cNLdlLvwhACezUBTg5XbZHR4Y.jpg"
+  },
+  {
+    "title": "Spider-Man",
+    "type": "series",
+    "imdbId": "tt6135682",
+    "id": "marvel_tt6135682",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/dKdcyyHUR5WTMnrbPdYN5y9xPVp.jpg"
+  },
+  {
+    "title": "Big Hero 6: The Series",
+    "type": "series",
+    "imdbId": "tt5515212",
+    "id": "marlvel_tt5515212",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/ldG4xXImCPyv5U5gbRmlB8On842.jpg"
+  },
+  {
+    "title": " Super Hero Adventures",
+    "type": "series",
+    "imdbId": "tt7726152",
+    "id": "marvel_tt7726152",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/9D5tShfIdGPo7sUB4vbe7qZ1DSx.jpg"
+  },
+  {
+    "title": "Spider-Man: Into the Spider-Verse",
+    "type": "movie",
+    "imdbId": "tt4633694",
+    "id": "marvel_tt4633694",
+    "releaseYear": "2018",
+    "poster": "https://image.tmdb.org/t/p/original/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg"
+  },
+  {
+    "title": "LEGO Avengers: Climate Conundrum",
+    "type": "series",
+    "imdbId": "tt13783758",
+    "id": "marvel_tt13783758",
+    "releaseYear": "2020",
+    "poster": "https://image.tmdb.org/t/p/original/yeCEBB9eLeJ4nmj00ePD7eqeWYx.jpg"
+  },
+  {
+    "title": "Battleworld",
+    "type": "series",
+    "imdbId": "tt12583300",
+    "id": "marlve_tt12583300",
+    "releaseYear": "2020",
+    "poster": "https://image.tmdb.org/t/p/original/7YSgKDJvpzWUHjdToca9GWoiHFd.jpg"
+  },
+  {
+    "title": "M.O.D.O.K.",
+    "type": "series",
+    "imdbId": "tt9811118",
+    "id": "marvel_tt9811118",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/2kXRzRjQZs9kAIm3K9MkQgsZR9C.jpg"
+  },
+  {
+    "title": "Super Crooks",
+    "type": "series",
+    "imdbId": "tt14272628",
+    "id": "marvel_tt14272628",
+    "releaseYear": "2021",
+    "poster": "https://upload.wikimedia.org/wikipedia/en/d/d6/Super_Crooks_anime_poster.webp"
+  },
+  {
+    "title": "What If...?",
+    "type": "series",
+    "imdbId": "tt10168312",
+    "id": "marvel_tt10168312",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/lztz5XBMG1x6Y5ubz7CxfPFsAcW.jpg"
+  },
+  {
+    "title": "LEGO Avengers: Loki in Training",
+    "type": "movie",
+    "imdbId": "tt21072140",
+    "id": "marvel_tt21072140",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/eyBifMbovoQbyuNXHOFYo7tsInp.jpg"
+  },
+  {
+    "title": "BayMax!",
+    "type": "series",
+    "imdbId": "tt13622958",
+    "id": "marvel_tt13622958",
+    "releaseYear": "2022",
+    "poster": "https://image.tmdb.org/t/p/original/muzgYvvFniQnwiTdAvfMCEkHsT4.jpg"
+  },
+  {
+    "title": "I Am Groot",
+    "type": "series",
+    "imdbId": "tt13623148",
+    "id": "marvel_tt13623148",
+    "releaseYear": "2022",
+    "poster": "https://image.tmdb.org/t/p/original/lNKkHTsjeVddRIq1XZ7kmtwtaZG.jpg"
+  },
+  {
+    "title": "LEGO Avengers: Time Twisted",
+    "type": "movie",
+    "imdbId": "tt16531868",
+    "id": "marvel_tt16531868",
+    "releaseYear": "2022",
+    "poster": "https://image.tmdb.org/t/p/original/7nA9AjJ8iZvbBPsFPC2FNoFj568.jpg"
+  },
+  {
+    "title": "Moon Girl and Devil Dinosaur",
+    "type": "series",
+    "imdbId": "tt8041658",
+    "id": "marvel_tt8041658",
+    "releaseYear": "2023",
+    "poster": "https://image.tmdb.org/t/p/original/1oFkjiF9fT2asKWjRiJtlbhu3hn.jpg"
+  },
+  {
+    "title": "Spider-Man: Across the Spider-Verse",
+    "type": "movie",
+    "imdbId": "tt8469492",
+    "id": "marvel_tt8469492",
+    "releaseYear": "2023",
+    "poster": "https://image.tmdb.org/t/p/original/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg"
+  },
+  {
+    "title": "LEGO Avengers: Code Red",
+    "type": "movie",
+    "imdbId": "tt28477867",
+    "id": "marvel_tt28477867",
+    "releaseYear": "2023",
+    "poster": "https://image.tmdb.org/t/p/original/rDzig50dj7VpLwJ7SThbamETK1G.jpg"
+  },
+  {
+    "title": "LEGO Avengers: Mission Demolition",
+    "type": "movie",
+    "imdbId": "tt33653255",
+    "id": "marvel_tt33653255",
+    "releaseYear": "2024",
+    "poster": "https://image.tmdb.org/t/p/original/goFO9r09JXotKRcawP46jhPX2H0.jpg"
+  },
+  {
+    "title": "Spidey and His Amazing Friends",
+    "type": "series",
+    "imdbId": "tt10862280",
+    "id": "marvel_tt10862280",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/dA4S18rnLx4sWW8agmLLKUvRM7n.jpg"
+  },
+  {
+    "title": "Hit-Monkey",
+    "type": "series",
+    "imdbId": "tt9811316",
+    "id": "marlve_tt9811316",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/1aK7hApRSDUv8xxDQXyQil7qBjN.jpg"
+  },
+  {
+    "title": "X-Men '97",
+    "type": "series",
+    "imdbId": "tt16026746",
+    "id": "marlve_tt16026746",
+    "releaseYear": "2024",
+    "poster": "https://image.tmdb.org/t/p/original/9Ycz7yYRf9V4jk3YXwcZhFtbNcF.jpg"
+  },
+  {
+    "title": "Your Friendly Neighborhood Spider-Man",
+    "type": "series",
+    "imdbId": "tt16027074",
+    "id": "marvel_tt16027074",
+    "releaseYear": "2025",
+    "poster": "https://image.tmdb.org/t/p/original/kjcsNeqF52YUQ2rUBGLMHwLkxvR.jpg"
+  },
+  {
+    "title": "Eyes of Wakanda",
+    "type": "series",
+    "imdbId": "tt13968252",
+    "id": "marlve_tt13968252",
+    "releaseYear": "2025",
+    "poster": "https://image.tmdb.org/t/p/original/b16lExo3HVcw2AWStyaFvAy5HFX.jpg"
+  },
+  {
+    "title": "Marvel Zombies",
+    "type": "series",
+    "imdbId": "tt16027014",
+    "id": "marvel_tt16027014",
+    "releaseYear": "2025",
+    "poster": "https://image.tmdb.org/t/p/original/jmGjVKBNxaQD2l0e4Jz5RbguRre.jpg"
+  }
+];
