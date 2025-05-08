153df8b12cfd6147a7565efa82cd0625e22af4e2
João <jpnapsp@gmail.com>
2025-05-08 20:52:10 +0100

    Update seriesData.js

--- Data/seriesData.js
+++ Data/seriesData.js
@@ -1,31 +1,195 @@
 module.exports = [
   {
-    "title": "Wolverine and the X-Men",
+    "title": "Spidey Super Stories",
     "type": "series",
-    "imdbId": "tt0772145",
-    "id": "marvel_tt0772145",
-    "releaseYear": "2009",
-    "poster": "https://image.tmdb.org/t/p/w500/oSeJ2nESB77iC6SLo4qcqoadjdt.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.0/10"
-      }
-    ]
+    "imdbId": "tt27826524",
+    "id": "marvel_tt27826524",
+    "releaseYear": "1974",
+    "poster": "https://image.tmdb.org/t/p/original/cSHETWKC1JPQ3cNmpMCx9vMPoBs.jpg"
   },
   {
-    "title": "Marvel Studios Legends",
+    "title": "The Amazing Spider-Man",
+    "type": "series",
+    "imdbId": "tt0076975",
+    "id": "marvel_tt0076975",
+    "releaseYear": "1977",
+    "poster": "https://image.tmdb.org/t/p/original/hJUka4mFGp9L3FNw9uJiUg6mkYG.jpg"
+  },
+  {
+    "title": "The Incredible Hulk",
+    "type": "series",
+    "imdbId": "tt0077031",
+    "id": "marvel_tt0077031",
+    "releaseYear": "1977",
+    "poster": "https://image.tmdb.org/t/p/original/8q7uFuNuMWTWfcpnCpBuZa3KZXx.jpg"
+  },
+  {
+    "title": "Night Man",
+    "type": "series",
+    "imdbId": "tt0128886",
+    "id": "marvel_tt0128886",
+    "releaseYear": "1977",
+    "poster": "https://image.tmdb.org/t/p/original/esqusThZ8KbEOT6NqIieocBk8gQ.jpg"
+  },
+  {
+    "title": "Mutant X",
+    "type": "series",
+    "imdbId": "tt0283203",
+    "id": "marvel_tt0283203",
+    "releaseYear": "2001",
+    "poster": "https://image.tmdb.org/t/p/w500/yiKg027jdpkSsguVBI9WqliNWez.jpg"
+  },
+  {
+    "title": "Blade: The Series",
+    "type": "series",
+    "imdbId": "tt0823333",
+    "id": "marvel_tt0823333",
+    "releaseYear": "2006",
+    "poster": "https://image.tmdb.org/t/p/original/pHMXi30FTFNLGzI0164b5GPECkE.jpg"
+  },
+  {
+    "title": "Marvel's Agents of S.H.I.E.L.D.",
+    "type": "series",
+    "imdbId": "tt2364582",
+    "id": "marvel_tt2364582",
+    "releaseYear": "2013",
+    "poster": "https://image.tmdb.org/t/p/w500/gHUCCMy1vvj58tzE3dZqeC9SXus.jpg"
+  },
+  {
+    "title": "WHIH Newsfront",
+    "type": "series",
+    "imdbId": "tt5296048",
+    "id": "marvel_tt5296048",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/original/2RAbC1bwYvE3kYr6PdYFUo6vmO6.jpg"
+  },
+  {
+    "title": "Marvel's Agent Carter",
+    "type": "series",
+    "imdbId": "tt3475734",
+    "id": "marvel_tt3475734",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/w500/fe79VYyLp5ZBstpJ4oukpuUT3B.jpg"
+  },
+  {
+    "title": "Powers",
+    "type": "series",
+    "imdbId": "tt1851040",
+    "id": "marvel_tt1851040",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/original/7imMR8ySLCPG3LsNlGej8GGtXBg.jpg"
+  },
+  {
+    "title": "Daredevil",
+    "type": "series",
+    "imdbId": "tt3322312",
+    "id": "marvel_tt3322312",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/w500/QWbPaDxiB6LW2LjASknzYBvjMj.jpg"
+  },
+  {
+    "title": "Jessica Jones",
+    "type": "series",
+    "imdbId": "tt2357547",
+    "id": "marvel_tt2357547",
+    "releaseYear": "2015",
+    "poster": "https://image.tmdb.org/t/p/w500/paf9wL3mOW9LT3ZjRxXqJcjeMEv.jpg"
+  },
+  {
+    "title": "Luke Cage",
+    "type": "series",
+    "imdbId": "tt3322314",
+    "id": "marvel_tt3322314",
+    "releaseYear": "2016",
+    "poster": "https://image.tmdb.org/t/p/w500/6R62oGGufhsf6DiMknCi047N0b7.jpg"
+  },
+  {
+    "title": "Marvel's Agents of S.H.I.E.L.D.: Slingshot",
+    "type": "series",
+    "imdbId": "tt6313900",
+    "id": "marvel_tt6313900",
+    "releaseYear": "2016",
+    "poster": "https://image.tmdb.org/t/p/w500/bwJej7WdmGRMCMyuDlotwAqVX7S.jpg"
+  },
+  {
+    "title": "Legion",
+    "type": "series",
+    "imdbId": "tt5114356",
+    "id": "marvel_tt5114356",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/4WLMWQgH3eXNP1aGrAuwlSiIx5j.jpg"
+  },
+  {
+    "title": "Iron Fist",
+    "type": "series",
+    "imdbId": "tt3322310",
+    "id": "marvel_tt3322310",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/original/4l6KD9HhtD6nCDEfg10Lp6C6zah.jpg"
+  },
+  {
+    "title": "The Defenders",
+    "type": "series",
+    "imdbId": "tt4230076",
+    "id": "marvel_tt4230076",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/w500/49XzINhH4LFsgz7cx6TOPcHUJUL.jpg"
+  },
+  {
+    "title": "Inhumans",
+    "type": "series",
+    "imdbId": "tt4154858",
+    "id": "marvel_tt4154858",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/w500/zKfGip55oJ9tdzhyd9ayGyFFhuo.jpg"
+  },
+  {
+    "title": "The Gifted",
+    "type": "series",
+    "imdbId": "tt4396630",
+    "id": "marvel_tt4396630",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/w500/nshCqszjTNuqhrB53vrSqWO18sE.jpg"
+  },
+  {
+    "title": "The Punisher",
+    "type": "series",
+    "imdbId": "tt5675620",
+    "id": "marvel_tt5675620",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/w500/c86aUAhACPyloPtL2CH4ZP5hO7V.jpg"
+  },
+  {
+    "title": "Runaways",
+    "type": "series",
+    "imdbId": "tt1236246",
+    "id": "marvel_tt1236246",
+    "releaseYear": "2017",
+    "poster": "https://image.tmdb.org/t/p/w500/zceMrfO17AQkeWJZNra4O90s1vE.jpg"
+  },
+  {
+    "title": "Cloak & Dagger",
+    "type": "series",
+    "imdbId": "tt5614844",
+    "id": "marvel_tt5614844",
+    "releaseYear": "2018",
+    "poster": "https://image.tmdb.org/t/p/w500/pYnRJuBPEqZO1o4fcxBTgmKNHfy.jpg"
+  },
+  {
+    "title": "Helstrom",
+    "type": "series",
+    "imdbId": "tt10266874",
+    "id": "marvel_tt10266874",
+    "releaseYear": "2020",
+    "poster": "https://image.tmdb.org/t/p/w500/5QmcKsepYpO9XYsWNLwsiULoaAu.jpg"
+  },
+  {
+    "title": "Marvel Studios: Legends",
     "type": "series",
     "imdbId": "tt13650480",
     "id": "marvel_tt13650480",
     "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/EpDuYIK81YtCUT3gH2JDpyj8Qk.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.5/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/original/EpDuYIK81YtCUT3gH2JDpyj8Qk.jpg"
   },
   {
     "title": "WandaVision",
@@ -33,27 +197,7 @@ module.exports = [
     "imdbId": "tt9140560",
     "id": "marvel_tt9140560",
     "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/sgCHdBJ1w0vJNUrW1Sq90KEXv9j.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.9/10"
-      }
-    ]
-  },
-  {
-    "title": "Marvel Studios Assembled",
-    "type": "series",
-    "imdbId": "tt14094206",
-    "id": "marvel_tt14094206",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/v2BHRwtQVkt5fssLdo5MpFgHJPY.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.9/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/sgCHdBJ1w0vJNUrW1Sq90KEXv9j.jpg"
   },
   {
     "title": "The Falcon and the Winter Soldier",
@@ -61,13 +205,15 @@ module.exports = [
     "imdbId": "tt9208876",
     "id": "marvel_tt9208876",
     "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/6kbAMLteGO8yyewYau6bJ683sw7.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.1/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/6kbAMLteGO8yyewYau6bJ683sw7.jpg"
+  },
+  {
+    "title": "Marvel Studios Assembled",
+    "type": "series",
+    "imdbId": "tt14094206",
+    "id": "marvel_tt14094206",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/original/50lfJlcUuR8Nit0YSudfK59EUM7.jpg"
   },
   {
     "title": "Loki",
@@ -75,41 +221,7 @@ module.exports = [
     "imdbId": "tt9140554",
     "id": "marvel_tt9140554",
     "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/oJdVHUYrjdS2IqiNztVIP4GPB1p.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.2/10"
-      }
-    ]
-  },
-  {
-    "title": "Spidey and His Amazing Friends",
-    "type": "series",
-    "imdbId": "tt10862280",
-    "id": "marvel_tt10862280",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/etO5jDS5WgR4Y1lTyXhQcilJ6u2.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "5.8/10"
-      }
-    ]
-  },
-  {
-    "title": "What If...?",
-    "type": "series",
-    "imdbId": "tt10168312",
-    "id": "marvel_tt10168312",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/lztz5XBMG1x6Y5ubz7CxfPFsAcW.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.3/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/oJdVHUYrjdS2IqiNztVIP4GPB1p.jpg"
   },
   {
     "title": "Hawkeye",
@@ -117,13 +229,23 @@ module.exports = [
     "imdbId": "tt10160804",
     "id": "marvel_tt10160804",
     "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/ct5pNE5dDHryHLDnxyZPYcqO1sz.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.5/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/ct5pNE5dDHryHLDnxyZPYcqO1sz.jpg"
+  },
+  {
+    "title": "Marvel Studios Legends",
+    "type": "series",
+    "imdbId": "tt13650480",
+    "id": "marvel_tt13650480",
+    "releaseYear": "2021",
+    "poster": "https://image.tmdb.org/t/p/w500/EpDuYIK81YtCUT3gH2JDpyj8Qk.jpg"
+  },
+  {
+    "title": "Marvel Lucha Libre Edition: El origen de la mascara",
+    "type": "series",
+    "imdbId": "tt23779108",
+    "id": "marvel_tt23779108",
+    "releaseYear": "2022",
+    "poster": "https://image.tmdb.org/t/p/original/lrSxTzfVZMig68bbX85za2gpxGb.jpg"
   },
   {
     "title": "Moon Knight",
@@ -131,13 +253,7 @@ module.exports = [
     "imdbId": "tt10234724",
     "id": "marvel_tt10234724",
     "releaseYear": "2022",
-    "poster": "https://image.tmdb.org/t/p/w500/vKDUmKO6F9bSKKyHhg7YGbgcEeF.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.3/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/vKDUmKO6F9bSKKyHhg7YGbgcEeF.jpg"
   },
   {
     "title": "Ms. Marvel",
@@ -145,27 +261,7 @@ module.exports = [
     "imdbId": "tt10857164",
     "id": "marvel_tt10857164",
     "releaseYear": "2022",
-    "poster": "https://image.tmdb.org/t/p/w500/3HWWh92kZbD7odwJX7nKmXNZsYo.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.2/10"
-      }
-    ]
-  },
-  {
-    "title": "I Am Groot",
-    "type": "series",
-    "imdbId": "tt13623148",
-    "id": "marvel_tt13623148",
-    "releaseYear": "2022",
-    "poster": "https://image.tmdb.org/t/p/w500/lYwFSsP4KsoRhOLhJvBFp3GEGEm.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.7/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/3HWWh92kZbD7odwJX7nKmXNZsYo.jpg"
   },
   {
     "title": "She-Hulk: Attorney at Law",
@@ -173,22 +269,7 @@ module.exports = [
     "imdbId": "tt10857160",
     "id": "marvel_tt10857160",
     "releaseYear": "2022",
-    "poster": "https://image.tmdb.org/t/p/w500/5xz2orV8f0usyrfGNshcoXHmiaV.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "5.2/10"
-      }
-    ]
-  },
-  {
-    "title": "Voices Rising: The Music of Wakanda Forever",
-    "type": "series",
-    "imdbId": "tt26345409",
-    "id": "marvel_tt26345409",
-    "releaseYear": "2023",
-    "poster": "https://image.tmdb.org/t/p/w500/tD0z0ONzplsO7JLkDk1cMTWR1Rd.jpg",
-    "ratings": []
+    "poster": "https://image.tmdb.org/t/p/w500/5xz2orV8f0usyrfGNshcoXHmiaV.jpg"
   },
   {
     "title": "Secret Invasion",
@@ -196,13 +277,7 @@ module.exports = [
     "imdbId": "tt13157618",
     "id": "marvel_tt13157618",
     "releaseYear": "2023",
-    "poster": "https://image.tmdb.org/t/p/w500/f5ZMzzCvt2IzVDxr54gHPv9jlC9.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "5.9/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/f5ZMzzCvt2IzVDxr54gHPv9jlC9.jpg"
   },
   {
     "title": "Echo",
@@ -210,27 +285,7 @@ module.exports = [
     "imdbId": "tt13966962",
     "id": "marvel_tt13966962",
     "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/vFyJH630cF68LohVYjQW49074Sy.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.0/10"
-      }
-    ]
-  },
-  {
-    "title": "X-Men '97",
-    "type": "series",
-    "imdbId": "tt16026746",
-    "id": "marvel_tt16026746",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/9Ycz7yYRf9V4jk3YXwcZhFtbNcF.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.8/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/vFyJH630cF68LohVYjQW49074Sy.jpg"
   },
   {
     "title": "Agatha All Along",
@@ -238,27 +293,7 @@ module.exports = [
     "imdbId": "tt15571732",
     "id": "marvel_tt15571732",
     "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/rwfDv05ck4McdvqdWU2sQVsQV2j.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.2/10"
-      }
-    ]
-  },
-  {
-    "title": "Your Friendly Neighborhood Spider-Man",
-    "type": "series",
-    "imdbId": "tt16027074",
-    "id": "marvel_tt16027074",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/kjcsNeqF52YUQ2rUBGLMHwLkxvR.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.7/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/rwfDv05ck4McdvqdWU2sQVsQV2j.jpg"
   },
   {
     "title": "Daredevil: Born Again",
@@ -266,13 +301,7 @@ module.exports = [
     "imdbId": "tt18923754",
     "id": "marvel_tt18923754",
     "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/9lLuhV703HGCbnz6FxnqCwIwzAZ.jpg",
-    "ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.5/10"
-      }
-    ]
+    "poster": "https://image.tmdb.org/t/p/w500/9lLuhV703HGCbnz6FxnqCwIwzAZ.jpg"
   },
   {
     "title": "Ironheart",
@@ -280,25 +309,22 @@ module.exports = [
     "imdbId": "tt13623126",
     "id": "marvel_tt13623126",
     "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/ha1kackPwP1BLtyibzuv5hABiDk.jpg",
-    "ratings": []
+    "poster": "https://image.tmdb.org/t/p/w500/ha1kackPwP1BLtyibzuv5hABiDk.jpg"
   },
   {
-    "title": "Eyes of Wakanda",
+    "title": "Wonder Man",
     "type": "series",
-    "imdbId": "tt13968252",
-    "id": "marvel_tt13968252",
+    "imdbId": "tt21066182",
+    "id": "marvel_tt21066182",
     "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/b16lExo3HVcw2AWStyaFvAy5HFX.jpg",
-    "ratings": []
+    "poster": "https://image.tmdb.org/t/p/w500/8uQZNewJkIsa5Yx1cWFgqBOTkQD.jpg"
   },
   {
-    "title": "Marvel Zombies",
+    "title": "Untitled Vision Series",
     "type": "series",
-    "imdbId": "tt16027014",
-    "id": "marvel_tt16027014",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/jmGjVKBNxaQD2l0e4Jz5RbguRre.jpg",
-    "ratings": []
-  }
+    "imdbId": "tt23112594",
+    "id": "marvel_tt23112594",
+    "releaseYear": "TBD",
+    "poster": null
+  },
 ];
